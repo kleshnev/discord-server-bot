99e1412e63769bc01cc26ba995d9238b386b8107
kleshnev <kleshnevden@mail.ru>
2023-06-21 21:03:41 +0400

    started migrating to slash commands

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const fetchGameIcon = require('./fetch.js');
 require('dotenv').config();
-const { Client, GatewayIntentBits, EmbedBuilder, MessageButton, MessageActionRow } = require('discord.js');
-
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits, EmbedBuilder, Collection, Events} = require('discord.js');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -10,6 +11,26 @@ const client = new Client({
     GatewayIntentBits.MessageContent
   ],
 });
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+
 // Event: Bot is ready
 client.on('ready', () => {
   const tokenAPI = process.env.ICON_API;
@@ -24,18 +45,38 @@ client.on('ready', () => {
     .catch(console.error);
 });
 
-client.on('messageCreate', (message) => {
-  if (message.content === 'ping') {
-    message.reply('Pong!')
-  }
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+  const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
+
+
 // Event: Message received
 client.on('messageCreate', async (message) => {
   // Check if the message starts with your bot's command prefix
   console.log(`Received message: ${message.content}`);
   const prefix = '!'; // Change this to your desired command prefix
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-
+  
+  let member = message.member;
+  let nickname = member.displayName;
   // Split the message content into command and arguments
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
@@ -64,23 +105,23 @@ client.on('messageCreate', async (message) => {
       return;
     }
 
-    const thumbnailUrl = await fetchGameIcon(gameName);
-
-    const exampleEmbed = new EmbedBuilder()
+    //const image = await fetchGameIcon(gameName);
+    const image = `https://i.imgur.com/d6KibHs.jpg`; 
+    const partyEmbed = new EmbedBuilder()
       .setColor(0x0099FF)
-      .setTitle('Some title')
-      .setThumbnail(thumbnailUrl)
+      .setTitle(`Собираем пати в игру ${gameName}!`)
       .addFields(
-        { name: 'Regular field title', value: 'Some value here' },
+        { name: 'Требуется игроков:', value: `${playersCount}` },
         { name: '\u200B', value: '\u200B' },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Создано пользователем', value: `${nickname}`, inline: true }
       )
-      .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-      .setImage('https://i.imgur.com/AfFp7pu.png')
+      .addFields(
+        {name: ``,value}
+      )
+      .setImage(image)
       .setTimestamp()
 
-    message.channel.send({ embeds: [exampleEmbed] });
+    message.channel.send({ embeds: [partyEmbed] });
   }
 
 });
